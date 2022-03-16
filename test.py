db97d7b5f3597a153908c6652daac2eda3bf4892
leittb <34034602+leittb@users.noreply.github.com>
2022-03-16 14:07:16 +0000

    task 2

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from websocket import create_connection
 try:
     ws = create_connection("wss://service.codechallenge.co.uk/socket")
     ws.send('connect|{"teamName":"TeamPyPeople","password":"PythonPeople"}')
+    ws.send('echo|HelloWorld')
+    while True:
+        result = ws.recv()
+        print(result)
 except Exception as ex:
     print("exception: ", format(ex))
     
