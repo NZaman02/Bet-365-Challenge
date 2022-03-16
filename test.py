74514b283a01cbf2a45460ebf007bbc568f06a9e
leittb <34034602+leittb@users.noreply.github.com>
2022-03-16 14:14:14 +0000

    csv

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-import json
 from websocket import create_connection
+import requests
 
 try:
     ws = create_connection("wss://service.codechallenge.co.uk/socket")
+    print("Connected")
     ws.send('connect|{"teamName":"TeamPyPeople","password":"PythonPeople"}')
-    ws.send('echo|HelloWorld')
-    while True:
-        result = ws.recv()
-        print(result)
+    print(ws.recv())
+    ws.send('matchlivegoals|')
+    result = ws.recv()
+    results = result.split("|")
+    url = results[1]
+    print(url)
+    response = requests.get(url)
+    print (response.status_code)
+    print (response.content)
 except Exception as ex:
     print("exception: ", format(ex))
     
